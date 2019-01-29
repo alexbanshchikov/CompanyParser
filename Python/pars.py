9a82ab5cd3f633069b803fa5db58e72505853a1c
alexbanshchikov <banshchikov.alex@outlook.com>
2019-01-29 14:05:47 +0700

    Added second(worked) & third requests.

--- Python/pars.py
+++ Python/pars.py
@@ -45,6 +45,50 @@ def parse_nalog(inn):
     file.write(response.content)  # записываем результат
     file.close()  # закрываем файл'''
 
+def parse_nalog_second_step():
+    url = "https://pb.nalog.ru/download-proc.json"
+
+    payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"params\"\r\n\r\ntoken=A6A58C340B10BBAC23C71BF238E3B8976D43D0D65124B7D18DB4D7692A9731C567AE3BF9F9B40C295F68C0FE01D412A8&inn=7021004633&pdf=vyp\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+    headers = {
+        'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        'cache-control': "no-cache",
+        'Postman-Token': "93f28bb7-e6bf-4f91-88c8-f984a519ac9a"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    print(response.text)
+
 # parse_zakupki(base_url, headers)
 
-parse_nalog(7021004633)
+#parse_nalog(7021004633)
+
+def parse_nalog_third():
+    url = "https://pb.nalog.ru/excerpt.pdf?token=CC5AE858100FB9D0FB9EFD7DF9FCC0D94323CC2CB91EC45DEFBB217768630D687A9DF6A09CF658303AB346B8301654BB61E04AFD3EE110AC708C1070B97137F9CC40374A9EE9152A07F0AA1AB5AD9A2ABE1A9BB98851AE16F8EC8D2A87EF41CA34F93B14BF8660A232309BDA33A704B69F61EF722A1F7EB1E15DBDF397EE4F65AA09AC45FAAEE25882FB55664504F9AEB9CD3C21D81998CBD894A348B3684565"
+
+   # querystring = {
+    #    "token": "CC5AE858100FB9D0FB9EFD7DF9FCC0D94323CC2CB91EC45DEFBB217768630D687A9DF6A09CF658303AB346B8301654BB61E04AFD3EE110AC708C1070B97137F9CC40374A9EE9152A07F0AA1AB5AD9A2ABE1A9BB98851AE16F8EC8D2A87EF41CA34F93B14BF8660A232309BDA33A704B69F61EF722A1F7EB1E15DBDF397EE4F65AA09AC45FAAEE25882FB55664504F9AEB9CD3C21D81998CBD894A348B3684565"}
+
+    payload = ""
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Referer': "https://pb.nalog.ru/company.html?token=A6A58C340B10BBAC23C71BF238E3B8976D43D0D65124B7D18DB4D7692A9731C567AE3BF9F9B40C295F68C0FE01D412A8",
+        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        'Accept-Encoding': "gzip, deflate, br",
+        'Accept-Language': "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+        'Cache-Control': "max-age=0",
+        'Connection': "keep-alive",
+        'Content-Length': "0",
+        'Host': "pb.nalog.ru",
+        'Origin': "https://pb.nalog.ru",
+        'Upgrade-Insecure-Requests': "1",
+        'User-Agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+        'cache-control': "no-cache",
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)#, params=querystring)
+
+    print(response.text)
+
+#parse_nalog_second_step()
+parse_nalog_third()

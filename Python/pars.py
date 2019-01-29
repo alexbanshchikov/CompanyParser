e50b320c18f66a5f7d7a26e41d13bb55c9e47d38
Bazarow Johny <bzrw87@gmail.com>
2019-01-29 17:36:36 +0700

    More fashionable code

--- Python/pars.py
+++ Python/pars.py
@@ -1,17 +1,6 @@
 import requests
+import time
 
-base_url = 'http://www.zakupki.gov.ru/epz/order/quicksearch/search.html?searchString=7018007264&strictEqual=on&pageNumber=1&sortDirection=false&recordsPerPage=_50&showLotsInfoHidden=false&fz44=on&fz223=on&ppRf615=on&af=on&ca=on&pc=on&pa=on&currencyId=-1&regionDeleted=false&sortBy=UPDATE_DATE'
-
-'''def parse_zakupki(base_url, headers):
-    session = requests.Session()
-    request = session.get(base_url, headers=headers)
-    if request.status_code == 200:
-        answer = bs(request.content, 'html.parser')
-        div1 = answer.find_all('div', attrs={'class':'registerBox registerBoxBank margBtm20'})
-        print(len(div1))
-    else:
-        print('ERROR')
-'''
 def parse_nalog(inn):
     url = "https://rmsp.nalog.ru/search-proc.json"
 
@@ -23,13 +12,6 @@ def parse_nalog(inn):
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     tokenOld = response.json().pop('data')[0].pop('token')
-
-    url = "https://pb.nalog.ru/download-proc.json"              # По этому запросу получаешь токен для Прозрачного Бизнеса
-    querystring = {"token": tokenOld, "inn": inn, "pdf": "vyp"}
-
-    response = requests.request("POST", url, body=querystring)
-    print(response.content)
-
                                                                 # Ниже нужно сделать GET запрос с новым токеном и также скачать pdf
     '''url = "https://pb.nalog.ru/excerpt.pdf"
 
@@ -44,35 +26,31 @@ def parse_nalog(inn):
     file = open('D://test_content2.pdf', 'wb')  # создаем файл для записи результатов
     file.write(response.content)  # записываем результат
     file.close()  # закрываем файл'''
+    return str(tokenOld)
 
-def parse_nalog_second_step():
+def parse_nalog_second_step(inn, tokenOld):
     url = "https://pb.nalog.ru/download-proc.json"
 
-    payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"params\"\r\n\r\ntoken=A6A58C340B10BBAC23C71BF238E3B8976D43D0D65124B7D18DB4D7692A9731C567AE3BF9F9B40C295F68C0FE01D412A8&inn=7021004633&pdf=vyp\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+    payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"params\"\r\n\r\ntoken=" + tokenOld + "&inn=" + str(inn) +"&pdf=vyp\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
     headers = {
         'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
         'cache-control': "no-cache",
-        'Postman-Token': "93f28bb7-e6bf-4f91-88c8-f984a519ac9a"
     }
 
     response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.json())
+    print(response.json()['token'])
+    return str(response.json()['token'])
 
-    print(response.text)
+def parse_nalog_third(tokenOld, tokenBig):
+    url = "https://pb.nalog.ru/excerpt.pdf?token=" + tokenBig
 
-# parse_zakupki(base_url, headers)
-
-#parse_nalog(7021004633)
-
-def parse_nalog_third():
-    url = "https://pb.nalog.ru/excerpt.pdf?token=CC5AE858100FB9D0FB9EFD7DF9FCC0D94323CC2CB91EC45DEFBB217768630D687A9DF6A09CF658303AB346B8301654BB61E04AFD3EE110AC708C1070B97137F9CC40374A9EE9152A07F0AA1AB5AD9A2ABE1A9BB98851AE16F8EC8D2A87EF41CA34F93B14BF8660A232309BDA33A704B69F61EF722A1F7EB1E15DBDF397EE4F65AA09AC45FAAEE25882FB55664504F9AEB9CD3C21D81998CBD894A348B3684565"
-
-   # querystring = {
-    #    "token": "CC5AE858100FB9D0FB9EFD7DF9FCC0D94323CC2CB91EC45DEFBB217768630D687A9DF6A09CF658303AB346B8301654BB61E04AFD3EE110AC708C1070B97137F9CC40374A9EE9152A07F0AA1AB5AD9A2ABE1A9BB98851AE16F8EC8D2A87EF41CA34F93B14BF8660A232309BDA33A704B69F61EF722A1F7EB1E15DBDF397EE4F65AA09AC45FAAEE25882FB55664504F9AEB9CD3C21D81998CBD894A348B3684565"}
+    querystring = {
+        "token": tokenBig}
 
     payload = ""
     headers = {
-        'Content-Type': "application/x-www-form-urlencoded",
-        'Referer': "https://pb.nalog.ru/company.html?token=A6A58C340B10BBAC23C71BF238E3B8976D43D0D65124B7D18DB4D7692A9731C567AE3BF9F9B40C295F68C0FE01D412A8",
+        'Referer': "https://pb.nalog.ru/company.html?token=" + tokenOld,
         'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
         'Accept-Encoding': "gzip, deflate, br",
         'Accept-Language': "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -86,9 +64,26 @@ def parse_nalog_third():
         'cache-control': "no-cache",
     }
 
-    response = requests.request("POST", url, data=payload, headers=headers)#, params=querystring)
+    response = requests.request("POST", url, headers=headers,  params=querystring)
 
     print(response.text)
 
-#parse_nalog_second_step()
-parse_nalog_third()
+tokenOld = parse_nalog(7021004633)
+#tokenBig = parse_nalog_second_step(7021004633, tokenOld)
+#parse_nalog_third(tokenOld, tokenBig)
+
+def Ex(tokenOld):
+    time1 = str(int(time.time()*1000))
+    url = 'https://egrul.nalog.ru/vyp-request/' + tokenOld + '?r=&_=' + time1
+
+    headers = {
+        'cache-control': "no-cache",
+    }
+    querystring = {
+        'r':'',
+        '_': time1
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
+
+Ex(tokenOld)

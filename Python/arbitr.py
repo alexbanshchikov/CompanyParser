610a3834a955d0d5cd6a01babb7cf1a53afa9b9c
alexbanshchikov <banshchikov.alex@outlook.com>
2019-02-05 13:22:57 +0700

    Changed arbitr_get_html() method's signature.

--- Python/arbitr.py
+++ Python/arbitr.py
@@ -1,15 +1,30 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
-def arbitr_get_html():
+def arbitr_get_html(params, count=25, vks="false"):
     url = "http://kad.arbitr.ru/Kad/SearchInstances"
 
-    payload = "{\n\t\"CaseNumbers\": [],\n\t\"Count\": 25,\n\t\"Courts\": [],\n\t\"DateFrom\": null,\n\t\"DateTo\": null,\n\t\"Judges\": [],\n\t\"Page\": 1,\n\t\"Sides\": \n\t\t[\n\t\t\t{\n\t\t\t\t\"Name\": \"7021004633\", \n\t\t\t\t\"Type\": -1, \n\t\t\t\t\"ExactMatch\": false\n\t\t\t\t\n\t\t\t}\n\t\t],\n\t\"WithVKSInstances\": false\n}"
+    payload = "{" \
+                  "CaseNumbers: " + str(params["CaseNumbers"]) + "," \
+                  "Count: " + str(count) + "," \
+                  "Courts: " + str(params["Courts"]) + "," \
+                  "DateFrom: " + str(params["DateFrom"]) + "," \
+                  "DateTo: " + str(params["DateTo"]) + "," \
+                  "Judges: " + str(params["Judges"]) + "," \
+                  "Page: " + str(params["Page"]) + "," \
+                  "Sides: " + str(params["Sides"]) + ","\
+                  "WithVKSInstances: " + str(vks) + "" \
+              "}"
     headers = {
         'Content-Type': "application/json",
-        'cache-control': "no-cache",
-        'Postman-Token': "f1509a00-989f-4ec3-af9f-0a764b33fa98"
+        'cache-control': "no-cache"
         }
 
     response = requests.request("POST", url, data=payload, headers=headers)
+    answer = bs(response.text, 'html.parser')
 
-    print(response.text)
+    print(answer)
+
+string = {"CaseNumbers": [],"Courts": [],"DateFrom": "null","DateTo": "null","Judges": [],"Page": 1,"Sides": [{"Name": 7021004633,"Type": -1,"ExactMatch": "false"}]}
+
+arbitr_get_html(params=string)

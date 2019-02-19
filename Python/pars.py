7375bae8ecd6c6eb477e813dc795a7aa5872ea27
alexbanshchikov <banshchikov.alex@outlook.com>
2019-02-19 15:09:30 +0700

    CleanUp.

--- Python/pars.py
+++ Python/pars.py
@@ -1,11 +1,4 @@
 import requests
-from pdfminer3.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer3.converter import HTMLConverter,TextConverter,XMLConverter
-from pdfminer3.layout import LAParams
-from pdfminer3.pdfpage import PDFPage
-from io import StringIO
-from io import BytesIO
-from bs4 import BeautifulSoup as bs
 from tabula import read_pdf
 import pandas
 
@@ -64,42 +57,16 @@ def parse_nalog_third(tokenBig):
     file.write(response.content)  # записываем результат
     file.close()  # закрываем файл'''
 
-#tokenOld = parse_nalog(7021004633)
-#tokenBig = parse_nalog_second_step(7021004633, tokenOld)
-#parse_nalog_third(tokenBig)
-
-def parse_pdf(path_to_file):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    #retstr = BytesIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    #device = HTMLConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(path_to_file, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
-
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
-                                  check_extractable=True):
-        interpreter.process_page(page)
-
-    text = retstr.getvalue()
-
-    fp.close()
-    device.close()
-    retstr.close()
-    return text
-
-#text = parse_pdf('D://vypiska.pdf')
+tokenOld = parse_nalog(7021004633)
+tokenBig = parse_nalog_second_step(7021004633, tokenOld)
+parse_nalog_third(tokenBig)
 
 files = "vypiska.pdf"
 path = 'D://vypiska.pdf'
+
 df = read_pdf(path, multiple_tables=True, encoding='utf-8', spreadsheet=True)#, pages='all')#, spreadsheet=True)
 print(df[1].iloc[8][1])
 print(df[1].iloc[8][2])
+
 #example = df[0]
 #print(example.values)
